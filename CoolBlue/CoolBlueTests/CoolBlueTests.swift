b716cb0c390da442be21d4ca126ac05b3d4c7953
Mohammad Allam <mohammada.allam@gmail.com>
2018-06-03 16:25:41 +0400

    Adding testing files

--- CoolBlue/CoolBlueTests/CoolBlueTests.swift
+++ CoolBlue/CoolBlueTests/CoolBlueTests.swift
@@ -7,30 +7,65 @@
 //
 
 import XCTest
+import RxSwift
+
 @testable import CoolBlue
 
 class CoolBlueTests: XCTestCase {
+    var serviceMock:productServiceMock!
+    var viewModel:SearchViewModel!
+    var disposeBag:DisposeBag!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        serviceMock = productServiceMock()
+        disposeBag = DisposeBag()
+        viewModel = SearchViewModel(inputService: serviceMock)
     }
     
     override func tearDown() {
+        viewModel = nil
+        serviceMock = nil
+        disposeBag = nil
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testCallingSearchMethod(){
+        // Given
+        let queryText = "iPhone"
+        let pageIndex = 1
+
+        // When
+        //Should be updating the "query" to make a new request
+        //Apparently that generates compilation error when running
+        ////        Observable.just("query")
+        ////            .bind(to: viewModel.inputs.query)
+        ////            .disposed(by: disposeBag)
+
+        // Then
+        XCTAssert(serviceMock.isDiscoverProductsCalled,"DiscoverProducts should have been called")
+        XCTAssertEqual(serviceMock.inputPage,pageIndex,"Requested page is different than than the expected")
+        XCTAssertEqual(serviceMock.inputQuery,queryText,"Requested query is different than than the expected")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testCallingLoadMoreMethod(){
+        // Given
+        let queryText = "iPhone"
+        let pageIndex = 2
+
+        // When
+        //Should be updating the "query" to make a new request
+        //Apparently that generates compilation error when running
+        ////        Observable.just(true)
+        ////            .bind(to: viewModel.inputs.loadMore)
+        ////            .disposed(by: disposeBag)
+
+        // Then
+        XCTAssert(serviceMock.isDiscoverProductsCalled,"DiscoverProducts should have been called")
+        XCTAssertEqual(serviceMock.inputPage,pageIndex,"Requested page is different than than the expected")
+        XCTAssertEqual(serviceMock.inputQuery,queryText,"Requested query is different than than the expected")
     }
-    
+
 }
--- CoolBlue/CoolBlueUITests/CoolBlueUITests.swift
+++ CoolBlue/CoolBlueUITests/CoolBlueUITests.swift
@@ -27,8 +27,48 @@ class CoolBlueUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
+
+    func testInitialState() {
+        let app = XCUIApplication()
+        let table = app.tables["SearchTable"]
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(table.cells.count,0, "There should be no cells")
+    }
+
+    func testEnteringSearchText() {
+
+        let app = XCUIApplication()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .searchField).element.tap()
+        app.searchFields.element.tap()
+        app.searchFields.element.typeText("iPhone")
+        sleep(5)
+        let table = app.tables["SearchTable"]
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertGreaterThanOrEqual(table.cells.count,1,"There should be more than one cell")
+    }
+
+    func testSelectProductToView() {
+        let app = XCUIApplication()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .searchField).element.tap()
+        app.searchFields.element.tap()
+        app.searchFields.element.typeText("iPhone")
+        app.scrollViews.otherElements.staticTexts["Sales price :1079.0"].tap()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testDetailedProductInfo() {
+
+        let app = XCUIApplication()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .searchField).element.tap()
+        app.searchFields.element.tap()
+        app.searchFields.element.typeText("Testing")
+        app.scrollViews.otherElements.containing(.staticText, identifier:"Apple iPhone X 64GB Space Gray").children(matching: .textView).element.tap()
+        app.buttons["Back"].tap()
+
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
